7b8b3292b0320050ce54eb563eeb544637236415
Roshellem24 <rmohan24@aoiths.org>
2023-11-03 12:02:18 -0400

    movement fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float horizontalInput;
+    public float xRange = 15;
+    public float speed = 10.0f;
+    public float verticalInput;
+    public float yRange = 10;
+
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-private float speed = 20.0f;
 
- private float turnSpeed = 45.0f;
+    // Update is called once per frame
+    void Update()
+    {
 
- private float horizontalInput;
-
- private float forwardInput;
-
- public bool hasCollectible;
-
- 
-
- // Update is called once per frame
- void Update()
- {
- horizontalInput = Input.GetAxis("Horizontal");
- forwardInput = Input.GetAxis("Vertical");
+        
+        //keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
 
 
- transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
- 
- transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
- }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (transform.position.z < -8)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -8);
+        }
+        if (transform.position.z > 8)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 8);
+        }
+
+    }
 
 
 }
