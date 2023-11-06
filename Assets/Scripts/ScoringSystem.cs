8dd81a3d0a3583b490f3f2e061f4385c5040a7cd
Roshellem24 <98101425+Roshellem24@users.noreply.github.com>
2023-11-06 14:21:51 -0500

    odg

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -8,10 +8,11 @@ public class ScoringSystem: MonoBehaviour
     public GameObject scoreText;
     public int theScore;
 
+
     void OnTriggerEnter(Collider other)
     {
         theScore += 50;
-        scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
+        
         Destroy(gameObject);
     }
 
